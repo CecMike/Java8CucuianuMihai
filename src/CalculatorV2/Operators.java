35f5ffdac9b11409b4021dd95d365bc1ed7e039e
CecMike <DarkMessia67@gmail.com>
2018-07-10 16:32:47 +0300

    Add files via upload

---
+++ src/CalculatorV2/Operators.java
@@ -0,0 +1,17 @@
+/*
+ * This class is responsible for getting the operation input from the user
+ * @author Mihai C
+ */
+package CalculatorV2;
+
+/**
+ *
+ * @author mike
+ */
+public class Operators extends Buttons{
+    
+     public Operators(String ButtonType) {
+        super(ButtonType);
+    }
+    
+}
